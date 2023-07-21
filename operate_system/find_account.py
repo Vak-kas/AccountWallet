3ecaab8b13275306b2cc5ee91d772e327d0a4025
서민재 <110338700+Vak-kas@users.noreply.github.com>
2023-07-21 17:03:21 +0900

    아이디 입력 시 비밀번호 출력

--- operate_system/find_account.py
+++ operate_system/find_account.py
@@ -82,17 +82,54 @@ def find_passward_using_list(lst):
 #사이트, 아이디 입력받으면 비밀번호 출력
 def find_user_passward():
     #사이트 입력받기, 엔터만 입력시 전체 계정 탐색
-    site_name = input("어떤 사이트의 id들을 불러올까요?(입력 없을 시 모든 사이트들의 계정을 불러옵니다)  >>  ").rstrip();
-    id_info = input("비밀번호를 알고 싶은 계정의 id를 입력하세요 >> ").rstrip();
-    id_name = id_info+".txt";
-    file_path = os.path.join(directory, dir_name, id_name); #파일 경로 설정
-    
-    #파일 경로에 파일이 존재하는가?
-    if os.isfile(file_path):
-        pass;
-    
+    print("|ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ|");
+    while True:
+        print("종료하려면 0를 입력하세요");
+        site_name = input("어떤 사이트인가요?(입력 없을 시 모든 사이트들의 계정을 불러옵니다)  >>  ").rstrip();
+        if site_name == "0":
+            break;
+        id_info = input("비밀번호를 알고 싶은 계정의 id를 입력하세요 >> ").rstrip();
+        if id_info == "0":
+            break;
+        id_name = id_info+".txt";
+        file_path = os.path.join(directory, site_name, id_name); #파일 경로 설정
+
+        #파일 경로에 파일이 존재하는가?
+        if os.path.isfile(file_path):
+            #파일 열기
+            with open(file_path, "r") as f:
+                data = f.readlines(); #데이터 전부를 읽어들임.
+                print(f"{id_info}[{data[2]}]의 비밀번호는")
+                print(f"{data[1][5:-1]}입니다.")
+                print("|ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ|");
+                #비밀번호 출력
+
     #파일 경로에 파일이 존재하지 않는가?
     else:
-        pass
+        print(f"{id_info}의 비밀번호는 존재하지 않습니다");
+        choose_flag = input(f"다른 곳에서 id로 '{id_info}'를 사용하는 사이트를 검색할까요? >> ");
+        #해당 계정을 사용하는 사이트를 검색 수락
+        if choose_flag == "Y" or choose_flag == "y":
+            id_list = find_all_id(None);
+            filter_lst = filter_id_list(id_list, id_name)
+            print(filter_lst);
+        
+        # 안 찾을 거니 메뉴화면으로 돌아감
+        else:
+            print("메뉴화면으로 돌아갑니다");
+            print("|ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ|");
+            
+            
+def filter_id_list(id_list, id_name):
+    result = [];
+    for i in id_list:
+        for j in i[1]:
+            if j == id_name:
+                result.append((i))
+                
+    return result;
+                
+            
+            
     
     
